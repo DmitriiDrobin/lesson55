36fddd385d61883f0d9fb80cce688e170f4bb58c
DmitriiDrobin <dmitrydrobin0626@gmail.com>
2025-01-19 20:19:59 +0300

    module_12_3.py
    123

--- module_12_3.py
+++ module_12_3.py
@@ -2,7 +2,6 @@ import unittest
 import module_12_1
 import module_12_2
 
-#Часть 1. TestSuit.
 M_test = unittest.TestSuite()
 M_test.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_1.RunnerTest))
 M_test.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_2.TournamentTest))
